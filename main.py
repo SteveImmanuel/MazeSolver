c1a1b372f7b4fdd39481341f847a75bd71230ea8
SteveImmanuel <iam.steve.immanuel@gmail.com>
2019-03-29 02:57:59 +0700

    fix visiting node problem

--- main.py
+++ main.py
@@ -22,15 +22,19 @@ class Grid:
         self.matrix=[]
         for i in range(height):
             self.matrix.append([None]*width)
-    def print(self):
+    def print(self,startNode,goalNode):
         for i in range(self.height):
             for j in range(self.width):
-                if(self.matrix[i][j].render==0):
+                if(self.matrix[i][j].isSameWith(startNode)):
+                    print(Back.RED+'  '+Style.RESET_ALL,end='')
+                elif(self.matrix[i][j].isSameWith(goalNode)):
+                    print(Back.GREEN+'  '+Style.RESET_ALL,end='')
+                elif(self.matrix[i][j].render==0):
                     print(Back.WHITE+'  '+Style.RESET_ALL,end='')
                 elif(self.matrix[i][j].render==1):
-                    print(Back.YELLOW+'  '+Style.RESET_ALL,end='')
+                    print(Back.BLUE+'  '+Style.RESET_ALL,end='')
                 elif(self.matrix[i][j].render==2):
-                    print(Back.RED+'  '+Style.RESET_ALL,end='')
+                    print(Back.YELLOW+'  '+Style.RESET_ALL,end='')
             print()
     def generateManDist(self,goalNode):
         for i in range(self.height):
@@ -40,9 +44,10 @@ class Grid:
         goalNode.render=2
         for i in range(len(goalNode.predecessor)):
             goalNode.predecessor[i].render=2
-    def removePath(self):
+    def reset(self):
         for i in range(self.height):
             for j in range(self.width):
+                self.matrix[i][j].visited=False
                 if(self.matrix[i][j].render==2):
                     self.matrix[i][j].render=0
 
@@ -54,8 +59,11 @@ class Node:
         self.value=0
         self.manhattandist=0
         self.predecessor=[]
+        self.visited=False
     def isSameWith(self,otherNode):
         return self.x==otherNode.x and self.y==otherNode.y
+    def setVisited(self,bool):
+        self.visited=bool
     # def print(self):
     #     print("     x="+str(self.x)+',y='+str(self.y))
 
@@ -93,14 +101,18 @@ def bfs(grid,startx,starty,endx,endy):
             grid.highLightPath(curNode)
             found=True
         else:
-            if(valid(grid,curNode,curNode.x-1,curNode.y) and not grid.matrix[curNode.x-1][curNode.y] in queue):
+            if(valid(grid,curNode,curNode.x-1,curNode.y) and  not grid.matrix[curNode.x-1][curNode.y].visited):
                 visit(curNode,grid.matrix[curNode.x-1][curNode.y],queue)
-            if(valid(grid,curNode,curNode.x,curNode.y-1) and not grid.matrix[curNode.x][curNode.y-1] in queue):
+                grid.matrix[curNode.x-1][curNode.y].setVisited(True)
+            if(valid(grid,curNode,curNode.x,curNode.y-1) and not grid.matrix[curNode.x][curNode.y-1].visited):
                 visit(curNode,grid.matrix[curNode.x][curNode.y-1],queue)
-            if(valid(grid,curNode,curNode.x+1,curNode.y) and not grid.matrix[curNode.x+1][curNode.y] in queue):
+                grid.matrix[curNode.x][curNode.y-1].setVisited(True)
+            if(valid(grid,curNode,curNode.x+1,curNode.y) and not grid.matrix[curNode.x+1][curNode.y].visited):
                 visit(curNode,grid.matrix[curNode.x+1][curNode.y],queue)
-            if(valid(grid,curNode,curNode.x,curNode.y+1) and not grid.matrix[curNode.x][curNode.y+1] in queue):    
+                grid.matrix[curNode.x+1][curNode.y].setVisited(True)
+            if(valid(grid,curNode,curNode.x,curNode.y+1) and not grid.matrix[curNode.x][curNode.y+1].visited):    
                 visit(curNode,grid.matrix[curNode.x][curNode.y+1],queue)
+                grid.matrix[curNode.x][curNode.y+1].setVisited(True)
     return [count,found]
 
 def astar(grid,startx,starty,endx,endy):
@@ -122,21 +134,22 @@ def astar(grid,startx,starty,endx,endy):
             grid.highLightPath(curNode)
             found=True
         else:
-            if(valid(grid,curNode,curNode.x-1,curNode.y) and not grid.matrix[curNode.x-1][curNode.y] in prioQueue.queue):
+            if(valid(grid,curNode,curNode.x-1,curNode.y) and not grid.matrix[curNode.x-1][curNode.y].visited):
                 grid.matrix[curNode.x-1][curNode.y].value=curNode.value+1
                 visit(curNode,grid.matrix[curNode.x-1][curNode.y],prioQueue)
-            if(valid(grid,curNode,curNode.x,curNode.y-1) and not grid.matrix[curNode.x][curNode.y-1] in prioQueue.queue):
+                grid.matrix[curNode.x-1][curNode.y].setVisited(True)
+            if(valid(grid,curNode,curNode.x,curNode.y-1) and not grid.matrix[curNode.x][curNode.y-1].visited):
                 grid.matrix[curNode.x][curNode.y-1].value=curNode.value+1
                 visit(curNode,grid.matrix[curNode.x][curNode.y-1],prioQueue)
-            if(valid(grid,curNode,curNode.x+1,curNode.y) and not grid.matrix[curNode.x+1][curNode.y] in prioQueue.queue):
+                grid.matrix[curNode.x][curNode.y-1].setVisited(True)
+            if(valid(grid,curNode,curNode.x+1,curNode.y) and not grid.matrix[curNode.x+1][curNode.y].visited):
                 grid.matrix[curNode.x+1][curNode.y].value=curNode.value+1
                 visit(curNode,grid.matrix[curNode.x+1][curNode.y],prioQueue)
-            if(valid(grid,curNode,curNode.x,curNode.y+1) and not grid.matrix[curNode.x][curNode.y+1] in prioQueue.queue):    
+                grid.matrix[curNode.x+1][curNode.y].setVisited(True)
+            if(valid(grid,curNode,curNode.x,curNode.y+1) and not grid.matrix[curNode.x][curNode.y+1].visited):    
                 grid.matrix[curNode.x][curNode.y+1].value=curNode.value+1
                 visit(curNode,grid.matrix[curNode.x][curNode.y+1],prioQueue)
-        # if (found):
-        #     prioQueue.removeUnwanted(curGoalNode)
-    # if (found):
+                grid.matrix[curNode.x][curNode.y+1].setVisited(True)
     return [count,found]
 
 
@@ -153,33 +166,33 @@ if __name__ == "__main__":
             for i in range(grid.height):
                 for j in range(grid.width):
                     grid.matrix[i][j]=Node(i,j,int(tempRow[i][j]))
-            print('Maze Input')
             colorama.init()
-            grid.print()
-            startx=int(input('Input x coordinate of start: '))
-            starty=int(input('Input y coordinate of start: '))
-            endx=int(input('Input x coordinate of goal: '))
-            endy=int(input('Input x coordinate of goal: '))
+            startx=int(input('Input row coordinate of start: '))
+            starty=int(input('Input column coordinate of start: '))
+            endx=int(input('Input row coordinate of goal: '))
+            endy=int(input('Input column coordinate of goal: '))
+            print('Maze Input')
+            grid.print(grid.matrix[startx][starty],grid.matrix[endx][endy])
             print('\nShortest Path using BFS Algorithm')
             start=time()
             result=bfs(grid,startx,starty,endx,endy)
             end=time()
             if(result[1]):
-                grid.print()
-                print('Total Iteration:',result[0])
+                grid.print(grid.matrix[startx][starty],grid.matrix[endx][endy])
             else:
                 print('No path found')
+            print('Total Iteration:',result[0])
             print('Time execution using BFS:',end-start,'s')
-            grid.removePath()
+            grid.reset()
             print('\nShortest Path using A* Algorithm')
             start=time()
             result=astar(grid,startx,starty,endx,endy)
             end=time()
             if(result[1]):
-                grid.print()
-                print('Total Iteration:',result[0])
+                grid.print(grid.matrix[startx][starty],grid.matrix[endx][endy])
             else:
                 print('No path found')
+            print('Total Iteration:',result[0])
             print('Time execution using A*:',end-start,'s')
         except:
             print('File',sys.argv[1],'not found')
